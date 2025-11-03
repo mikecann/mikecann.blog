3655ac2db61c814e9accc09db7ae727b1c994a6c
Mike Cann <mike.cann@gmail.com>
2025-11-03 13:50:19 +0800

    wip

--- components/mikebot/messages/MessageRow.tsx
+++ components/mikebot/messages/MessageRow.tsx
@@ -1,16 +1,9 @@
-import { Grid, Horizontal, Vertical } from "../../utils/gls";
 import * as React from "react";
-import { Doc } from "../../../convex/_generated/dataModel";
-import { BubbleMessageContent } from "./BubbleMessageContent";
 import { bouncy } from "ldrs";
-import { HorizontalSpacer } from "gls";
-import { Message } from "@convex-dev/agent/validators";
 import { type UIMessage } from "@convex-dev/agent/react";
-import { ToolMessage } from "./ToolMessage";
 import { AssistantMessage } from "./AssistantMessage";
 import { UserMessage } from "./UserMessage";
 import { exhaustiveCheck } from "../../../essentials/misc/misc";
-import { SystemMessage } from "./SystemMessage";
 
 interface Props {
   message: UIMessage;
