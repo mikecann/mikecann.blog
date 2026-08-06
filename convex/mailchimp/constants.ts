d93f42aab0b0d1c564e808847f23312cadd973ea
Mike Cann <mike.cann@gmail.com>
2026-08-06 15:56:24 +0800

    Increase new post email delay to 30 minutes

--- convex/mailchimp/constants.ts
+++ convex/mailchimp/constants.ts
@@ -1,4 +1,4 @@
 // Give the production site time to finish deploying before subscribers receive
 // links to a newly created post. Production deploys currently take around seven
-// minutes, so fifteen minutes leaves a useful safety margin.
-export const NEW_POST_EMAIL_DELAY_MS = 15 * 60 * 1000;
+// minutes, so thirty minutes leaves a generous safety margin.
+export const NEW_POST_EMAIL_DELAY_MS = 30 * 60 * 1000;
